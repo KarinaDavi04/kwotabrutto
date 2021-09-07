5cecedd07e9aa40a12d197ee85cadf19135a79f3
KarinaDavi04 <90253669+KarinaDavi04@users.noreply.github.com>
2021-09-07 13:33:05 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	float kwotabrutto;
+	cout<<"Prosze podac kwote btutto "<<endl;
+	cin>>kwotabrutto;
+	cout<<"kwota podatku wynosi \t"<< kwotabrutto*0.19<<endl;
+	cout<<"kwota netto wynosi \t"<< kwotabrutto - kwotabrutto*0.19<<endl;
+	return 0;
+}
